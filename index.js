8436ca3aea0a23e881c0b8de7cd352b52c3ce6f5
Ibrahima <ibrahimajalloh@gmail.com>
2023-11-06 10:52:16 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(game => game.result === "W");
+  
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  // Example usage:
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    // Add more game records here
+  ];
+  
+  const winYear = superbowlWin(record);
+  console.log("Superbowl win year:", winYear);
+  
